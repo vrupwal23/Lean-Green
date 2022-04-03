3db6544e1b6afe6c29ad0772178527b164478978
vrupwal23 <102917795+vrupwal23@users.noreply.github.com>
2022-04-03 11:42:52 -0500

    Create route2.py

---
+++ route2.py
@@ -0,0 +1,57 @@
+from route3 import route_3
+from functions import is_correct2
+from functions import roll_two_dice
+
+def route_2(userInput2):
+  while (userInput2 == 1 or userInput2 == 2 ):
+    if(userInput2 == 1):
+      diceRolloutcome = roll_two_dice()
+      if (diceRolloutcome < 6):
+        print("You got {:.0f}, which is not high enough of a roll to take the jump. Would you like to roll for the jump or roll to survive? \n1- Roll to jump\n2- Roll to survive the night\n".format(diceRolloutcome))
+        userInput2= int(input("Pick one of the numbers from above:\t"))
+        userInput2 = is_correct2(userInput2)
+        if(userInput2 == 2):
+          print("You chose to wait around. Roll a die to see if you survived the night or to jump. If you get higher than 6 than you survive. \n1- Roll to jump\n2- Roll to survive the night\n")
+          userInput2= int(input("Pick one of the numbers from above:\t"))
+          userInput2 = is_correct2(userInput2)
+          diceRolloutcome = roll_two_dice()
+          if(diceRolloutcome < 6):
+            print("You were eaten by a bear. The End.")
+            userInput2 = 4
+          else:
+            print("You survived the night. You can roll to take a jump or wait. \n1- Roll to jump\n2- Roll to survive the night\n")
+            userInput2= int(input("Pick one of the numbers from above:\t"))
+            userInput2 = is_correct2(userInput2)
+        else:
+          diceRolloutcome = roll_two_dice()
+          if (diceRolloutcome < 6):
+            print("You got {:.0f}, which is not high enough of a roll to take the jump. Would you like to roll for the jump or roll to survive? \n1- Roll to jump\n2- Roll to survive the night\n".format(diceRolloutcome))
+            userInput2= int(input("Pick one of the numbers from above:\t"))
+            userInput2 = is_correct2(userInput2)
+          else:
+            userInput2 = 3
+      else:
+        userInput2 = 3
+    else:
+      print("You survived the night. You can roll to take a jump or wait. \n1- Roll to jump\n2- Roll to survive the night\n")
+      userInput2= int(input("Pick one of the numbers from above:\t"))
+      userInput2 = is_correct2(userInput2)
+      if (userInput2 == 1):
+        diceRolloutcome = roll_two_dice()
+        if (diceRolloutcome < 6):
+          print("You got {:.0f}, which is not high enough of a roll to take the jump. Would you like to roll for the jump or roll to survive? \n1- Roll to jump\n2- Roll to survive the night\n".format(diceRolloutcome))
+          userInput2= int(input("Pick one of the numbers from above:\t"))
+          userInput2 = is_correct2(userInput2)
+        else:
+          userInput2 = 3
+      else:
+        diceRolloutcome = roll_two_dice()
+        if(diceRolloutcome < 6):
+          print("You were eaten by a bear. The End.")
+          userInput2 = 4
+        else:
+          print("You survived the night. You can roll to take a jump or wait. \n1- Roll to jump\n2- Roll to survive the night\n")
+          userInput2= int(input("Pick one of the numbers from above:\t"))
+          userInput2 = is_correct2(userInput2)
+  if(userInput2 == 3):
+    route_3()
